d5db996d0424272733c8f82f036b66ea196410bb
naytozero <zusumillaa@gmail.com>
2020-10-26 11:17:54 +0000

    leo

---
+++ ejercicio 1.py
@@ -0,0 +1,17 @@
+importar al  azar
+
+def  calcularInversionFinAnho ():
+    interes = 0.02
+    contadorMes = 0
+    interesMes = 0
+    #Datos de entrada
+    montoInvertir = float ( input ( "Ingrese el monto a invertir:" ))
+    #Proceso
+    while ( contadorMes < 12 ):
+        interesMes = montoInvertir * interes
+        montoInvertir = montoInvertir + interesMes
+        contadorMes = contadorMes + 1
+    #Datos de salida
+    
+    print ( f "El monto de dinero que tiene al cabo de un anho es: { montoInvertir } " )
+    print ( "El monto de dinero que tiene al cabo de un anho es:" , montoInvertir )
