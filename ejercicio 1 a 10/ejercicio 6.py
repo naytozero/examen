71e538d3f796fd5e9d6b3f1ea5c51a60f4b0f5ab
naytozero <zusumillaa@gmail.com>
2020-10-27 21:08:23 +0000

    leo

---
+++ ejercicio 1 a 10/ejercicio 6.py
@@ -0,0 +1,8 @@
+cantidad=float(input("ingrese la inversion inicial :"))
+años = float(input("ingrese los años  :"))
+cantidad=cantidad * años
+años=años * 0.10
+interes=cantidad*años 
+print ("el interes es  ",cantidad * 0.10)
+
+print ("el saldo total es de  :" ,interes )
